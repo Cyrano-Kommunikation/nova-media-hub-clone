462432f698162580dee62312382ebd094018d600
oaky <81476563+oakyydev@users.noreply.github.com>
2023-01-13 13:35:40 +0100

    Added background for white mode

--- resources/js/modals/EditMediaModal.vue
+++ resources/js/modals/EditMediaModal.vue
@@ -26,7 +26,7 @@
             </div>
           </div>
           <div
-            class="o1-w-full dark:o1-bg-gray-900 o1-z-10 o1-rounded o1-border-gray-300 o1-border dark:o1-border-gray-700 o1-mt-2 o1-p-2">
+            class="o1-w-full dark:o1-bg-gray-900 o1-bg-gray-500 o1-z-10 o1-rounded o1-border-gray-300 o1-border dark:o1-border-gray-700 o1-mt-2 o1-p-2">
             <span
               class="o1-text-xs o1-px-3 o1-py-1 o1-bg-slate-700 o1-m-1 o1-rounded-xl o1-inline-block"
               v-for="(tag, index) in selectedTags" :key="tag">
@@ -44,7 +44,7 @@
               Benutzergruppen auswählen
             </Button>
             <div v-show="showRoleDropdown"
-                 class="o1-w-[394px] o1-absolute o1-z-[1000] o1-top-[-60px] o1-max-h-[150px] dark:o1-bg-gray-900 o1-z-10 o1-rounded o1-border-gray-300 o1-border dark:o1-border-gray-700 o1-mt-2 shadow o1-overflow-hidden o1-overflow-y-scroll"
+                 class="o1-w-[394px] o1-absolute o1-z-[1000] o1-top-[-60px] o1-max-h-[150px] dark:o1-bg-gray-900 o1-bg-gray-500 o1-z-10 o1-rounded o1-border-gray-300 o1-border dark:o1-border-gray-700 o1-mt-2 shadow o1-overflow-hidden o1-overflow-y-scroll"
                  v-if="filterRoles.length > 0">
               <div class="o1-p-4 cursor-pointer o1-border-b o1-border-gray-300 dark:o1-border-gray-700"
                    v-for="role in filterRoles"
